0c2a7716b759e2c3fbe158d36f50e6bfefe494c6
smashmouthallstar <wharf-shun-yiddish@duck.com>
2026-05-17 17:22:33 -0400

    Create initializeGrid function

---
+++ script.js
@@ -0,0 +1,12 @@
+let gridNum = 16;
+
+const container = document.querySelector("#container");
+
+function initializeGrid(gridNum) {
+	for (let i = 1; i < gridNum + 1; i++) {
+		const div = document.createElement("div");
+		container.appendChild(div);
+	}
+}
+
+initializeGrid(gridNum);
