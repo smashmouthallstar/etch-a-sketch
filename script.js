c61c7b3b932d12c726d3e7245a66de9a79b9efd2
smashmouthallstar <wharf-shun-yiddish@duck.com>
2026-05-17 17:34:01 -0400

    Correct number of divs to add
    Number of divs should equal gridNum**2 because otherwise you only have
    16 squares instead of 16*16 squares.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let gridNum = 16;
 const container = document.querySelector("#container");
 
 function initializeGrid(gridNum) {
-	for (let i = 1; i < gridNum + 1; i++) {
+	for (let i = 1; i < gridNum**2 + 1; i++) {
 		const div = document.createElement("div");
 		container.appendChild(div);
 	}
