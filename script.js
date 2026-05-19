103910ff4cb0ed1c21b595171543f77bd626bbfe
smashmouthallstar <wharf-shun-yiddish@duck.com>
2026-05-19 14:42:55 -0400

    Allow drawing

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ const container = document.querySelector("#container");
 function initializeGrid(gridNum) {
 	for (let i = 1; i < gridNum**2 + 1; i++) {
 		const div = document.createElement("div");
+		div.addEventListener("mouseover", (event) => {
+			event.target.classList.add("colored");
+		});
 		container.appendChild(div);
 	}
 }
+
+
 
 initializeGrid(gridNum);
