eded25f40cc975e810ed28b16bae0dd81a25b489
smashmouthallstar <wharf-shun-yiddish@duck.com>
2026-05-19 18:46:47 -0400

    Add prompt safeguards

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ const container = document.querySelector("#container");
 
 const sheet = document.styleSheets[0];
 
+function getGrids() {
+	while (true) {
+	answer = +prompt("How many squares do you want? (Default 16)", 16)
+		if (answer > 100) {
+			alert("Please enter a value <= 100");
+			continue;
+		} else if (isNaN(answer) || !Number.isInteger(answer) || !answer) {
+			alert("Please enter a valid number");
+			continue;
+		} else return answer;
+	}
+}
+
+
 function initializeGrid(gridNum) {
 	for (let i = 1; i < gridNum**2 + 1; i++) {
 		const div = document.createElement("div");
@@ -24,7 +38,7 @@ function resetGrid() {
 
 gridSetter.addEventListener("click", () => {
 	resetGrid();
-	gridNum = prompt("How many squares do you want? (Default 16)");
+	gridNum = getGrids();
 	console.log(`New gridNum = ${gridNum}x${gridNum} (${gridNum**2})`);
 	sheet.deleteRule(5);
 	sheet.insertRule(`#container > div {display: flex; width: calc(100%/${gridNum});}`, 5);
